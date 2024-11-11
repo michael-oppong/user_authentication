1066666bc60aa336cf91f15c7ffbaf4b37b871fa
Michael Oppong <michaelopponghero@gmail.com>
2024-11-11 13:53:44 +0000

    improved
    mend

--- dbconfig.php
+++ dbconfig.php
@@ -2,7 +2,5 @@
     $conn = new mysqli('localhost', 'root', '', 'attendance');
     if($conn -> error){
         echo 'Error: Connection was not successful';
-    }else {
-        echo "success";
     }
 ?>
